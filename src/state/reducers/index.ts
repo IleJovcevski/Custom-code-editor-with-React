368068421b0dec11bb98e5d2329ca605ec234f41
Ilija Jovchevski <ilija.jovchevski@webfactory.mk>
2024-11-25 14:25:42 +0100

    Creating and exporting the store

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,1 +1,10 @@
-export {};
+import { combineReducers } from "redux";
+import cellsReducer from "./cellsReducer";
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+//applying types to redux
+export type RootState = ReturnType<typeof reducers>;
