017a9aa7787020dabbc513e7fac7f27f24ead17f
Ilija Jovchevski <ilija.jovchevski@webfactory.mk>
2024-11-28 11:52:35 +0100

    Refactored Redux

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -45,7 +45,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       state.order[targetIndex] = action.payload.id;
 
       return state;
-    case ActionType.INSERT_CEL_BEFORE:
+    case ActionType.INSERT_CEL_AFTER:
       const cell: Cell = {
         content: "",
         type: action.payload.type,
@@ -58,9 +58,9 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       );
 
       if (foundIndex < 0) {
-        state.order.push(cell.id);
+        state.order.unshift(cell.id);
       } else {
-        state.order.splice(foundIndex, 0, cell.id);
+        state.order.splice(foundIndex + 1, 0, cell.id);
       }
 
       return state;
